6c53c33363cc6811b234d05e3b2749037f372c8e
kn-nalbantov <77404315+kn-nalbantov@users.noreply.github.com>
2021-02-25 18:40:22 +0200

    Add files via upload

---
+++ lang/eng.php
@@ -0,0 +1,14 @@
+<?php
+
+$lang['lang-eng'] = 'English';
+$lang['lang-it'] = 'Italian';
+$lang['lang-bg'] = 'Bulgarian';
+
+$lang['index-title'] = 'Dio gallery';
+$lang['index-h1'] = 'Gallery';
+$lang['index-lamatta'] = 'Dare to be different! La Matta takes excess and injected it with style.';
+$lang['index-kaos'] = 'Never banal. Kaos - colourful, original and playful.';
+$lang['index-rudger'] = 'For the remarkable man who is a cut above the rest.';
+$lang['index-learn-more'] = 'Learn more.'
+
+?>
