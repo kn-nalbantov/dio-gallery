62377ec3f8749bfc3b1892655dd58581bd5facfa
kn-nalbantov <77404315+kn-nalbantov@users.noreply.github.com>
2021-02-25 18:41:18 +0200

    Add files via upload

---
+++ lang/it.php
@@ -0,0 +1,14 @@
+<?php
+
+$lang['lang-eng'] = 'Inglese';
+$lang['lang-it'] = 'Italiano';
+$lang['lang-bg'] = 'Bulgaro';
+
+$lang['index-title'] = 'Dio galleria';
+$lang['index-h1'] = 'Galleria';
+$lang['index-lamatta'] = 'LA MATTA L’INTERPRETAZIONE DELL’ECCESSO “CON STILE”.';
+$lang['index-kaos'] = 'Mai banale. Kaos - coloratissimo, originale e scherzoso.';
+$lang['index-rudger'] = 'Per l\'uomo straordinario che è una spanna sopra gli altri.';
+$lang['index-learn-more'] = 'Per saperne di più.';
+
+?>
